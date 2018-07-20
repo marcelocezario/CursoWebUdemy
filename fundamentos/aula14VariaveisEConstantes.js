b44da23bfef70eb95b2e5c0208a3475b47dd5029
Marcelo Cezário <marcelocezario@gmail.com>
2018-07-20 04:49:38 -0300

    Primeiras aulas: fundamentos js

---
+++ fundamentos/aula14VariaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4
+
+var a = 30 // é possível declarar novamente var em js
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50 - não é possível alterar valor de uma constante
+console.log(c)
